0b3bd32a34a60be4ed879e22b0438b1761c1393a
apil <078bct017.apil@pcampus.edu.np>
2023-08-15 22:27:32 +0545

    Modified reference position of board and spwan

--- inc/globals.hpp
+++ inc/globals.hpp
@@ -2,8 +2,15 @@
 
 constexpr int CELL_SIZE = 30;
 
-constexpr int X_BOARD = 483;
-constexpr int Y_BOARD = 297;
+constexpr int ROWS = 10;
+constexpr int COLUMNS = 20;
+
+constexpr int X_BOARD = 17 * (CELL_SIZE+1);
+constexpr int Y_BOARD = (CELL_SIZE + 1);
+
+constexpr int X_SPAWN = X_BOARD + 4 * (CELL_SIZE+1);
+constexpr int Y_SPAWN = Y_BOARD;
+
 
 
 
